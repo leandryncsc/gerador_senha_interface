719f531cfdb229823e455dfdab4f7a033a134191
leandryncsc <leandrinhodoema@gmail.com>
2024-07-05 09:55:22 -0300

    inclusao de log e de visualizador

--- gerador_senha.py
+++ gerador_senha.py
@@ -7,13 +7,17 @@ def gerar_senha():
     comprimento = int(entry_comprimento.get())
     caracteres = string.ascii_letters + string.digits + string.punctuation
     senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
-    entry_senha.delete(0, tk.END)
-    entry_senha.insert(0, senha)
+
+    # Exibir a senha gerada na interface
+    label_senha_gerada['text'] = senha
+    
+    # Gravar a senha gerada no arquivo log.txt
+    with open('log.txt', 'a') as f:
+        f.write(senha + '\n')
 
 def copiar_para_area_de_transferencia():
-    senha = entry_senha.get()
     janela.clipboard_clear()
-    janela.clipboard_append(senha)
+    janela.clipboard_append(label_senha_gerada['text'])
     messagebox.showinfo("Copiar", "Senha copiada para a área de transferência!")
 
 # Criando a janela principal
@@ -32,8 +36,8 @@ botao_gerar = tk.Button(janela, text="Gerar Senha", command=gerar_senha)
 botao_gerar.pack(pady=10)
 
 # Entry para exibir a senha gerada
-entry_senha = tk.Entry(janela, width=50)
-entry_senha.pack(pady=5)
+label_senha_gerada = tk.Label(janela, text="", fg="green",width=50)
+label_senha_gerada.pack(pady=5)
 
 # Botão para copiar a senha para a área de transferência
 botao_copiar = tk.Button(janela, text="Copiar Senha", command=copiar_para_area_de_transferencia)
